e03863de6903f439e103b567d0f6220116ba4760
jgraykeyin <71823844+jgraykeyin@users.noreply.github.com>
2022-01-30 21:22:22 -0330

    Added better commenting and fixed list removal crash

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from typing import List
 import enchant
 import random
 import itertools
 
 # Setup an alphabet for each letter slot
+# The returned hints will help clear out unused letters
 wordle = [
     ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
     ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
@@ -17,6 +19,7 @@ starters = ["soare","roate","raise","party"]
 # Grab a random start word
 first_try = random.choice(starters)
 
+# Container for catching all the 'yellow' hints, this will be used for filtering out words
 yellows = []
 
 
@@ -25,6 +28,7 @@ def userColorInput(word):
     word_data = []
     count=0
     for i in word:
+        # Ask the user what hint was returned for each letter
         print("1) Correct [green]")
         print("2) Wrong place [yellow]")
         print("3) Not in word [black]")
@@ -33,6 +37,7 @@ def userColorInput(word):
             count+=1
             status = int(input("Status of letter #{} -> {}: ".format(count,i.upper())))
 
+            # Input validation so it'll make sure 1,2 or 3 was entered
             if status >= 1 and status <= 3:
                 word_data.append({
                     "letter":i.lower(),
@@ -45,6 +50,7 @@ def userColorInput(word):
     return word_data
 
 
+# Function for processing the hints returned
 def processAttempt(data):
     global wordle
     global yellows
@@ -52,21 +58,28 @@ def processAttempt(data):
     count=0
     for d in data:
         if d["status"] == 1:
+            # Green
+            # Replace the entire alphabet with the correct letter for this slot
             wordle[count] = d["letter"]
         elif d["status"] == 2:
+            # Yellow
+            # Remove this letter from the current slot's alphabet
             if wordle[count]:
                 wordle[count].remove(d["letter"])
+                # Also add it to the yellow tracker
                 if d["letter"] not in yellows:
                     yellows.append(d["letter"])
         elif d["status"] == 3:
+            # Remove this letter from every slot's alphabet
             i=0
             while i < 5:
-                if wordle[i]:
+                if wordle[i] == List:
                     wordle[i].remove(d["letter"])
                 i+=1
         count+=1
 
 
+# Function for generating list of possible words for the next choice
 def generateNextChoice():
     global wordle
     global yellows
@@ -74,20 +87,24 @@ def generateNextChoice():
     # Initialize the dictionary
     d = enchant.Dict("en_US")
 
+    # Placeholders for all generated words and a solution
     solution = ["","","","",""]
     possible_words = []
 
-    # Place all the corrects into the solution slots
+    # This function could take a few minutes, patience!
+    print("Thinking...")
+
+    # Place all the green correct letters into the solution slots
     count=0
     for w in wordle:
         if len(w) == 1:
             solution[count] = w
         count+=1
 
-    # Get all possible combinations of letters
+    # Get all possible combinations of letters remaining from each slot's alphabet
     combos = list(itertools.product(*wordle))
 
-    # Run through the combinations and check to see if it's a word
+    # Run through each combination and check to see if it's a word
     for combo in combos:
         word = ""
         x=0
@@ -95,14 +112,17 @@ def generateNextChoice():
             word += combo[x]
             x+=1
         if d.check(word) == True:
+            # If it's an actual word, let's add it to the placeholder
             possible_words.append(word)
 
+    # Check all the possible words and filter out any that don't contain yellow letters
     filtered_words = []
     if yellows:
         for word in possible_words:
             print(word)
             check = any(yellow in word for yellow in yellows)
             if check == True:
+                # If the word contains a yellow, move it to the new filtered word list
                 filtered_words.append(word)
             
 
@@ -113,8 +133,11 @@ def generateNextChoice():
 # User will have to enter this word into the game so we can get our hints.
 print("\n*** First try: {} ***\n".format(first_try.upper()))
 
+# Ask the user for the returned hints
 word_data = userColorInput(first_try)
 
+# Start processing the attempt
 processAttempt(word_data)
 
+# Generate a list of possible choices for next try
 next_choices = generateNextChoice()
